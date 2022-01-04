eaa23a79843f1f45fec52109013ee52b2194cffa
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-04 22:06:42 +0100

    file starting the application

---
+++ run.py
@@ -0,0 +1,9 @@
+from app_folder import create_app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host='0.0.0.0')
