20dc43f5d74a33ef0e3b4bf167b7c303f965b87c
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-05 19:27:10 +0100

    Adding before request to get endpoint name in order to set active clas to the link

--- app_folder/blueprints/users/views.py
+++ app_folder/blueprints/users/views.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, request, g
 from app_folder.blueprints.users import user
+
+@user.before_request 
+def get_current_blueprint():
+    g.name = request.endpoint
+    g.active = 'active'
 
 @user.route('/login')
 def login():
     return render_template('login.html')
+
+
+@user.route('/logout')
+def logout():
+    return render_template('logout.html')
