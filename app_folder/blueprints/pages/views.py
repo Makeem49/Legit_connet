4d5900728af114937d93991292bf6f794aa2d9ee
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-05 19:25:30 +0100

    Adding before request to get blueprint name and set class active

--- app_folder/blueprints/pages/views.py
+++ app_folder/blueprints/pages/views.py
@@ -1,9 +1,18 @@
-from flask import render_template
+from flask import render_template, request, g
 from app_folder.blueprints.pages import page
+
+
+@page.before_request
+def get_current_blueprint():
+    g.name = request.endpoint
+    g.active = 'active'
 
 @page.route('/')
 @page.route('/home')
 def home():
-    login = None
+    login = True
+    active = 'active'
+    current_page = g.name
+    print(current_page)
     return render_template('home.html', login=login)
     
