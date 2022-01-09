88ffde3762497d9affee262d72c5cf84128fa764
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-09 17:37:22 +0100

    Adding model and user

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
 from app_folder import create_app
+from app_folder.blueprints.users.model import User
+from app_folder.extensions import db 
 
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, User=User, db=db)
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
