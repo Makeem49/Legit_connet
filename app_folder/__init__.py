41ef14ae93cba46a276d4a248a2d1fa4832d8b4c
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-13 23:29:29 +0100

    adding extension for mail and migration

--- app_folder/__init__.py
+++ app_folder/__init__.py
@@ -1,50 +1,12 @@
-import os
+from celery import Celery 
 
-from flask import Flask
-from app_folder.blueprints.pages import page
-from app_folder.blueprints.users import user
-from app_folder.blueprints.auth import auth
-from app_folder.blueprints.error import error
 
-from app_folder.extensions import db 
-from app_folder.extensions import csrf
+def make_celery(app_name = __name__):
+    redis_uri='redis://localhost:6379'
+    return Celery(app_name, backend=redis_uri, broker=redis_uri)
 
+
+celery = make_celery()
 
 
 
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # registering blueprint
-    app.register_blueprint(page)
-    app.register_blueprint(user)
-    app.register_blueprint(auth)
-
-    # initialising extension
-    extensions(app)
-
-
-
-
-    return app
-
-
-def extensions(app):
-    db.init_app(app)
-    csrf.init_app(app)
