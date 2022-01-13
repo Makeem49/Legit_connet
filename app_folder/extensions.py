9508f18d10a8af3d2123b304e9390c98b70b0c42
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-13 23:37:10 +0100

    Adding flask-mail and flask-migrate extension

--- app_folder/extensions.py
+++ app_folder/extensions.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
+from flask_mail import Mail
+
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+migrate = Migrate()
+mail = Mail()
 
 
