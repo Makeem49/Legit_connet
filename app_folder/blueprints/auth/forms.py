a2a3b6627cc8ce12ae95fe7d6a7e575035950d53
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-13 23:30:02 +0100

    adding ariel data attribute

--- app_folder/blueprints/auth/forms.py
+++ app_folder/blueprints/auth/forms.py
@@ -1,30 +1,31 @@
 from flask_wtf import  FlaskForm
-from sqlalchemy.orm.mapper import validates
-from flask_wtf import Form
 from wtforms import StringField, PasswordField, RadioField, EmailField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import InputRequired, EqualTo, Email
+
+def custom_keywords():
+    return {"aria-describedby":"inputGroupPrepend3 validationServerUsernameFeedback"}
 
 
 class RegisterForm(FlaskForm):
-    surname = StringField('Surname', [DataRequired()])
-    name = StringField('First name', [DataRequired()])
-    email = EmailField('Email', [DataRequired(), Email(message='Email is required')])
-    password = PasswordField("password", [DataRequired()])
-    confirm_password = PasswordField("Confirm pasword", [ DataRequired(), EqualTo('password', message='Password must match') ])
+    surname = StringField('Surname', [InputRequired()], render_kw=custom_keywords())
+    name = StringField('First name', [InputRequired()], render_kw=custom_keywords())
+    email = EmailField('Email', [InputRequired(), Email(message='Email is required')], render_kw=custom_keywords())
+    password = PasswordField("password", [InputRequired()])
+    confirm_password = PasswordField("Confirm pasword", [ InputRequired(), EqualTo('password', message='Password must match') ])
     gender = RadioField('Gender', choices=['male', 'female'])
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email', [DataRequired(), Email(message='Email is required')])
-    password = PasswordField("Password", [DataRequired()])
+    email = EmailField('Email', [InputRequired(), Email(message='Email is required')])
+    password = PasswordField("Password", [InputRequired()])
     remember_me = BooleanField('Rememeber me')
     submit = SubmitField("Sign in")
 
 
 class ForgetPassword(FlaskForm):
-    email = EmailField('Email', [DataRequired(), Email(message='Email is required')])
+    email = EmailField('Email', [InputRequired(), Email(message='Email is required')])
     submit = SubmitField('Request password reset link')
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField("password", [DataRequired()])
-    confirm_password = PasswordField("Confirm pasword", [ DataRequired(), EqualTo('password', message='Password must match') ])
+    password = PasswordField("password", [InputRequired()])
+    confirm_password = PasswordField("Confirm pasword", [ InputRequired(), EqualTo('password', message='Password must match') ])
