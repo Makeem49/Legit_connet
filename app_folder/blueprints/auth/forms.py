83770f4449ea8397e4fa216c784148827be53071
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-23 00:17:04 +0100

    adding update email and validation

--- app_folder/blueprints/auth/forms.py
+++ app_folder/blueprints/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import  FlaskForm
 from wtforms import StringField, PasswordField, RadioField, EmailField, SubmitField, BooleanField, DateField
 from wtforms.validators import InputRequired, EqualTo, Email, ValidationError, Optional
 from app_folder.blueprints.users.model import User
+from flask_login import current_user
 
 def custom_keywords():
     return {"aria-describedby":"inputGroupPrepend3 validationServerUsernameFeedback"}
@@ -14,12 +15,17 @@ class RegisterForm(FlaskForm):
     password = PasswordField("password", [InputRequired()])
     confirm_password = PasswordField("Confirm pasword", [ InputRequired(), EqualTo('password', message='Password must match') ])
     gender = RadioField('Gender', [InputRequired()] ,choices=['male', 'female'])
+    username = StringField('Username', [InputRequired()])
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user: 
             raise ValidationError('There is a user with email account.')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('There is a user with this username.')
 
 class LoginForm(FlaskForm):
     email = EmailField('Email', [InputRequired(), Email(message='Email is required')])
@@ -35,3 +41,15 @@ class ForgetPassword(FlaskForm):
 class ResetPasswordForm(FlaskForm):
     password = PasswordField("password", [InputRequired()])
     confirm_password = PasswordField("Confirm pasword", [ InputRequired(), EqualTo('password', message='Password must match') ])
+
+
+class UpdateEmailAddress(FlaskForm):
+    new_email = EmailField('New email', [InputRequired()])
+    confirm_email = EmailField('Confirm email', [InputRequired(), EqualTo('new_email', message='Password must match')])
+    password = PasswordField('Password', [InputRequired()])
+
+    def validate_password(self, password):
+        user = User.query.filter_by(email=current_user.email).first()
+        if not user.verify_password(password.data):
+            raise ValidationError(f'Password not correct, we need to verify {current_user.username} authorise this update')
+            
