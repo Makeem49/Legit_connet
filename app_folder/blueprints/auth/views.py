80210980c561b356c76709f887d84f196c332264
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-23 00:18:09 +0100

    adding the update email route

--- app_folder/blueprints/auth/views.py
+++ app_folder/blueprints/auth/views.py
@@ -1,8 +1,7 @@
 from crypt import methods
-from multiprocessing.dummy import current_process
 from flask import request, render_template, g, redirect, url_for, flash
 from app_folder.blueprints.auth import auth
-from app_folder.blueprints.auth.forms import RegisterForm, LoginForm, ForgetPassword, ResetPasswordForm
+from app_folder.blueprints.auth.forms import RegisterForm, LoginForm, ForgetPassword, ResetPasswordForm, UpdateEmailAddress
 from app_folder.blueprints.users.model import User
 from app_folder.extensions import db
 from app_folder.email import send_mail
@@ -10,6 +9,16 @@ import os
 from lib.safe_next_url import is_safe_url
 from flask_login import login_user, login_required, logout_user, current_user
 from lib.custom_token import serializer
+from lib.verify_login import already_logged_in
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen()
+    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[0:5] != 'auth.':  
+        return redirect(url_for('auth.unconfirm'))
+
 
 @auth.before_request 
 def get_current_blueprint():
@@ -17,6 +26,7 @@ def get_current_blueprint():
     g.active = 'active'
 
 @auth.route('/login', methods=['GET', 'POST'])
+@already_logged_in
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -24,12 +34,13 @@ def login():
         user = db.session.query(User).filter_by(email=email).first()
         if user and user.verify_password(form.password.data):
             next_page = request.args.get('next')
+            print(next_page)
             if login_user(user, remember=form.remember_me.data) and user.is_active():
                 if next_page:
                     return redirect(is_safe_url(next_page))
                 else:
                     flash('You have successfully logged in', 'success')
-                    return redirect(url_for('user.profile'))
+                    return redirect(url_for('user.settings'))
             else:
                 flash("Your account has been deactivated, please contact the admin re-access your account.", 'info')
         elif user is None:
@@ -40,6 +51,7 @@ def login():
 
 
 @auth.route('/register', methods=['GET', 'POST'])
+@already_logged_in
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -50,8 +62,9 @@ def register():
             email = form.email.data
             password = form.password.data
             gender = form.gender.data
+            username = form.username.data.lower()
             user = User(surname=surname, first_name=first_name, email=email, password=password, gender=gender, 
-                                                        session_token=serializer().dumps([surname, password]))
+                                                        session_token=serializer().dumps([surname, password]), username=username)
             db.session.add(user)
             db.session.commit()
             token = user.generate_confirmation_token()
@@ -64,6 +77,7 @@ def register():
     return render_template('register.html', form=form)
 
 @auth.route('/forget_password', methods=['GET', 'POST'])
+@already_logged_in
 def forget_password():
     if current_user.is_authenticated:
         return redirect(url_for('user.profile'))
@@ -83,9 +97,8 @@ def forget_password():
 
 
 @auth.route('/reset/<token>', methods=['GET', 'POST'])
+@already_logged_in
 def password_reset(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('page.profile'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         new_password = form.password.data
@@ -114,12 +127,8 @@ def confirm(token):
         flash('You account has been confirmed', 'success')
     else: 
         flash("Invalid token or token expired", 'error')
-    return redirect(url_for('user.profile'))
+    return redirect(url_for('user.profile', username=current_user.username))
 
-@auth.before_app_request
-def before_request():
-    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[0:5] != 'auth.':  
-        return redirect(url_for('auth.unconfirm'))
     
 @auth.route('unconfirm')
 def unconfirm():
@@ -137,3 +146,27 @@ def resend_confirmation():
     send_mail.delay(current_user.email, 'Account Confirmation', 'mail/registration', token=token)
     flash('Activation link sent.', 'success')
     return redirect(url_for('auth.unconfirm'))
+
+@auth.route('/update_email', methods=['GET', 'POST'])
+@login_required
+def update_email_address():
+    form = UpdateEmailAddress()
+    if form.validate_on_submit():
+        email = form.new_email.data
+        token = current_user.generate_token_for_new_email_address(email)
+        print(f"{token} new token ---> is printed out.")
+        send_mail.delay(email, 'Legit Email Reset', 'mail/change_email', name=current_user.first_name, token=token)
+        flash('A link has been been sent to your new email address, please confirm by clicking it.', 'success')
+        return redirect(url_for('user.settings'))
+    return render_template('update_email.html', form=form)
+
+@auth.route('/comfirm_email/<token>')
+@login_required
+def confirm_email_address(token):
+    confirm = current_user.confirm_new_email_address(token)
+    if confirm:
+        flash('Your email has been update', 'success')
+        return redirect(url_for('user.settings'))
+    else:
+        flash('Invalid or expired token.', 'info')
+        return redirect(url_for('user.settings'))
