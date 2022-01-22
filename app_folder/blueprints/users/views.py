0fecbd9968a41687c54d646e69f509ed0f5a81db
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-22 10:04:11 +0100

    committing users changes

--- app_folder/blueprints/users/views.py
+++ app_folder/blueprints/users/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import redirect, render_template, url_for, flash 
 from app_folder.blueprints.users import user
 from flask_login import login_required, current_user
 from app_folder.blueprints.users.forms import UpdateCredentials
+from app_folder.extensions import db 
 
 
 
@@ -9,7 +10,7 @@ from app_folder.blueprints.users.forms import UpdateCredentials
 def profile():
     return render_template('profile.html')
 
-@user.route('/settings')
+@user.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
     form = UpdateCredentials()
@@ -21,10 +22,13 @@ def settings():
         current_user.about_me = form.about_me.data
         current_user.date_of_birth  = form.about_me.data
         current_user.username = form.username.data
+        db.session.commit()
+        flash('Profile updated', 'success')
+        return redirect(url_for('user.profile'))
     form.surname.data = current_user.surname
     form.name.data = current_user.first_name
     form.username.data = current_user.username
-    form.password.data = "hoifhewoihihf;lqe"
+    form.password.data = "hoifhewoihihflqe"
     form.email.data = current_user.email
     form.course.data = current_user.course
     form.education.data = current_user.education
