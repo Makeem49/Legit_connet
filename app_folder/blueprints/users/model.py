bc4d4891cf259492b3dfff37a1926aed1435812f
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-24 08:49:14 +0100

    Adding permission class and anonymous class

--- app_folder/blueprints/users/model.py
+++ app_folder/blueprints/users/model.py
@@ -1,10 +1,13 @@
+from email.policy import default
 from http import server
+from numbers import Integral
 from app_folder.extensions import db 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.types import Text
 from werkzeug.security import generate_password_hash, check_password_hash
 from app_folder.extensions import login_manager
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -17,6 +20,43 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENT = 0x08
+    ADMINISTER = 0x80
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    default = Column(Boolean, server_default='false', index=True)
+    permissions = Column(Integer)
+    users = relationship('User', back_populates='role')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User' : (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES, True),
+            "Moderator" : (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES | Permission.MODERATE_COMMENT , False),
+            'Admin' : (0xff, False)
+        }
+
+        for role in roles:
+            role_instance = Role.query.filter_by(name=role).first()
+            if role_instance is None:
+                role_instance = Role(name=role)
+                role_instance.default = roles[role][1]
+                role_instance.permissions = roles[role][0]
+                db.session.add(role_instance)
+        db.session.commit()
+
+    def __repr__(self) :
+        return f"Role {self.name}"
+            
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -34,7 +74,7 @@ class User(db.Model, UserMixin):
     last_seen = Column(DateTime, default=datetime.utcnow)
     confirmed = Column(Boolean, server_default='f', nullable=False)
 
-    # Experience
+    # Experience 
     job_title = Column(String(250), nullable=True, server_default='')
     education = Column(String(100), nullable=True, server_default='')
     course = Column(String(100), nullable=True, server_default='')
@@ -43,6 +83,18 @@ class User(db.Model, UserMixin):
     # custom token 
     session_token = Column(Text, nullable=False, server_default='', unique=True)
 
+    # relationship 
+    roles_id = Column(Integer, ForeignKey('roles.id'))
+    
+    role = relationship('Role', back_populates='users')
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['ADMIN_EMAIL']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            else: 
+                self.role = Role.query.filter_by(default=True).first()      
 
     @property
     def password(self):
@@ -114,7 +166,7 @@ class User(db.Model, UserMixin):
             return False
 
         self.email = data.get('new_email')
-        db.session.add(self.email)
+        db.session.add(self)
         db.session.commit()
         return True
 
@@ -122,15 +174,31 @@ class User(db.Model, UserMixin):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
         db.session.commit()
-    
+
+    def can(self, permissions):
+        if self.role is not None and (self.role.permissions & permissions) == permissions:
+            return True
+
+    def is_admin(self):
+        if self.can(Permission.ADMINISTER):
+            return True
+        return False
+
+    @staticmethod
+    def add_admin():
+        admin = User(surname='Admin', first_name='Account', password='password', 
+                        email="admin@gmail.com", gender='male', confirmed=True, username='Admin123')
+        db.session.add(admin)
+        db.session.commit()
+        print('Admin added to the successfully.')
     
     def __repr__(self):
         return f'{self.surname} {self.first_name} is created.'
 
-        
 
+class MyAnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
 
-    
-
-
-
+    def is_admin(self):
+        return False
