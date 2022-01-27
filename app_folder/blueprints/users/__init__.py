48ec282d4cb89c0430a2ffe43f819ebaa2d76193
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-27 08:56:26 +0100

    inject Permission class to template

--- app_folder/blueprints/users/__init__.py
+++ app_folder/blueprints/users/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from .model import Permission
 
 user = Blueprint('user', __name__, template_folder="templates", url_prefix='/user')
 
 from app_folder.blueprints.users import views
+
+@user.app_context_processor
+def inject_permisstion():
+    return dict(Permission=Permission)
