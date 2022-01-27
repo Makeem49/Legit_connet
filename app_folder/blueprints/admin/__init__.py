2e76ef14a6d2224e56cfc650714e6e516466fb0f
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-27 08:53:35 +0100

    adding prefix url to admin blueprint

--- app_folder/blueprints/admin/__init__.py
+++ app_folder/blueprints/admin/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-admin = Blueprint('admin', __name__, template_folder='templates')
+admin = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
 
 from . import views
