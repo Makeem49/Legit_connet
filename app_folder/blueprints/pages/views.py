7b84c3374faa4f5c39832af6f0f2df45386dcccb
makeem49 <moshoodakeemolayinka@gmail.com>
2022-02-06 00:53:49 +0100

    adding posts to the home page with pagination

--- app_folder/blueprints/pages/views.py
+++ app_folder/blueprints/pages/views.py
@@ -1,6 +1,13 @@
-from flask import render_template, request, g
+from crypt import methods
+from traceback import print_tb
+from flask import current_app, render_template, request, g, redirect , url_for
 from app_folder.blueprints.pages import page
 from app_folder.blueprints.users.model import User
+from .forms import PostForm
+from app_folder.extensions import db 
+from flask_login import login_required, current_user
+from .models import Post
+from lib.permissions import Permission
 
 
 @page.before_request
@@ -8,9 +15,19 @@ def get_current_blueprint():
     g.name = request.endpoint
     g.active = 'active'
 
-@page.route('/')
-@page.route('/home')
+
+
+@page.route('/', methods=['GET', 'POST'])
+@page.route('/home',  methods=['GET', 'POST'])
 def home():
-    user = User.query.first()
-    return render_template('home.html', user=user)
-    
+    form = PostForm()
+    page = request.args.get('page', 1 , int)
+    print(request.args.get('page'))
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(content = form.content.data, user = current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('page.home'))
+    pagination = Post.query.order_by(Post.date_posted.desc()).paginate(page, current_app.config['LEGIT_POST_PER_PAGE'], error_out=False)
+    posts = pagination.items
+    return render_template('home.html', posts=posts, form=form, pagination=pagination)
