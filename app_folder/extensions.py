df4a89ef2c65ff7b952fedd443ff8fbc49adaad1
makeem49 <moshoodakeemolayinka@gmail.com>
2022-02-06 12:32:34 +0100

    adding moment extension

--- app_folder/extensions.py
+++ app_folder/extensions.py
@@ -3,6 +3,8 @@ from flask_wtf.csrf import CSRFProtect
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_moment import Moment
+
 
 
 login_manager = LoginManager()
@@ -10,5 +12,4 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 migrate = Migrate()
 mail = Mail()
-
-
+moment = Moment()
