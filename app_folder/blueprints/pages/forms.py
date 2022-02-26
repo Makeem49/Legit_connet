8aafe1dc286b024c36c4a2da246f8e272911208a
makeem49 <moshoodakeemolayinka@gmail.com>
2022-02-26 23:01:44 +0100

    adding selectfield

--- app_folder/blueprints/pages/forms.py
+++ app_folder/blueprints/pages/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm 
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, SelectField
 from wtforms.validators import InputRequired
 
 
 class PostForm(FlaskForm):
     content = TextAreaField('Content', [InputRequired()])
+    hash_tag = SelectField('Category', choices=[('all', "All"),
+        ("world", "World"),
+        ("u.s", "U.S"),
+        ("technology", "Technology"),
+        ("design", "Design"),
+        ("culture", "Culture"),
+        ("business", "Business"),
+        ("politics", "Politics"),
+        ("opinion", "Opinion"),
+        ("science", "Science"),
+        ("style", "Style"),
+        ("travel", "Travel")
+        ])
     submit = SubmitField('Post')
